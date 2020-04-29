3bede8b13093eb2d347f51ba9f6bed0c8640282c
mahesh0431 <p.maheshkumarreddy@gmail.com>
2020-04-29 16:06:11 +0000

    first

---
+++ app/v2/books/Component.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("v2.books.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
